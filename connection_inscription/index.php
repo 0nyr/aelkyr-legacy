a1eb8ff0ea6ee6b9db622d739862f148f55d920a
onyr <rascoussier.florian@gmail.com>
2019-08-27 11:29:01 +0200

    @0nyr: preview, inscription en cours de dev

---
+++ connection_inscription/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aelkyr, fait par sharklouis</title>
+</head>
+<body>
+<!--Insérer ici le code-source d'Onyr--->
+	<p><a href="inscription.php">S'inscrire</a> et se porter consentant pour toutes les souffrances engendrées par cette action irréversible.</p>
+	<br /><p><a href="connexion.php">Se connecter</a> et poursuivre l'aventure (et aussi se faire poursuivre par des monstres).</p>
+</body>
+</html>
